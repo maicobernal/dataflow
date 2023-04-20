18106fa34776ba33acaadd30211b93a314007c41
Maico Bernal <maico@MacBook-Pro-de-Maico.local>
2023-04-20 11:04:10 -0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(
-    name='beam-flex-demo',
+    name='pipeline-trial',
     version='1.0',
     install_requires=[],
     packages=setuptools.find_packages(),
     include_package_data=True,
 
     # warnings suppressors
-    description='Beam Flex Demo',
-    author='KP',
-    author_email='kp@apache.beam',
+    description='Pipeline on Dataflow Demo',
+    author='MB',
+    author_email='MB@apache.beam',
     url='https://foo.bar/',)
